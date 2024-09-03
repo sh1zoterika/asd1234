93d760d8d32942b36fc30d6eb6ea0104e02965bd
sh1zoterika <hatekillhate@mail.ru>
2024-09-03 07:35:42 +0300

    исправил addproductwindow

--- main/AddProductWindow.py
+++ main/AddProductWindow.py
@@ -20,7 +20,7 @@ class AddProductWindow(BaseProductWindow):
             'insert': """INSERT INTO Order_items (order_id, product_name, quantity, price) VALUES (%s, %s, %s, %s)"""
         }
         headers = ['Товар', 'Количество', 'Цена', 'Количество в заказ']
-        super().__init__('Добавить товары в заказ', (600, 200, 1000, 600), headers, query, parent, self.user, self.password)
+        super().__init__('Добавить товары в заказ', (600, 200, 1000, 600), headers, query, self.user, self.password, parent)
         self.order_id = order_id
 
         # Button to add products to the order
@@ -28,3 +28,29 @@ class AddProductWindow(BaseProductWindow):
         self.add_button.clicked.connect(self.add_products_to_order)
         layout = self.centralWidget().layout()
         layout.addWidget(self.add_button)
+
+    def add_products_to_order(self):
+        warehouse_id = self.combo_box.currentData()
+        if warehouse_id:
+            try:
+                connection = self.connect_db()
+                cursor = connection.cursor()
+                connection.autocommit = False
+
+                for i in range(self.warehouse_table.rowCount()):
+                    product_name = self.warehouse_table.item(i, 0).text()
+                    quantity = int(self.order_table.item(i, 0).text())
+                    price = float(self.warehouse_table.item(i, 2).text())
+                    if quantity > 0:
+                        cursor.execute(self.query['insert'], (self.order_id, product_name, quantity, price))
+
+                connection.commit()
+                connection.close()
+                QMessageBox.information(self, 'Успех', 'Товары добавлены в заказ.')
+            except Exception as e:
+                if connection:
+                    connection.rollback()
+                print(f"Error adding products to order: {e}")
+                QMessageBox.critical(self, 'Ошибка', f'Ошибка добавления товаров в заказ: {e}')
+        else:
+            QMessageBox.warning(self, 'Ошибка', 'Пожалуйста, выберите склад.')
